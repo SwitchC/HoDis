4db24cfc1538ac207dc3fb1add37cf8c433c739f
Hordiichuk Vitalii <vitalikokgo@gmail.com>
2026-03-31 23:43:52 +0300

    Add check_pass logic with 60% threshold

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def calculate_score(correct, total):
     if total == 0:
         return 0
     return round((correct / total) * 100, 2)
+
+def check_pass_status(score):
+    threshold = 60 
+    return score >= threshold
